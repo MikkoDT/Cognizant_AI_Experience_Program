d10ddfcddf2d6e8740f83cfd821e2c80e3e92425
MikkoDT <mikko.detorres@g.batstate-u.edu.ph>
2023-07-31 21:13:49 +0800

    Add folder Task4

---
+++ Task4/Task4.py
@@ -0,0 +1,36 @@
+# Section1 - Import libraries
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
+from sklearn.preprocessing import StandardScaler
+
+# Section2 - Load data
+def load_data(path: str = "/path/to/csv/"):
+    """
+    This function takes a path string to a CSV file and loads it into
+    a Pandas DataFrame.
+
+    :param      path (optional): str, relative path of the CSV file
+
+    :return     df: pd.DataFrame
+    """
+
+    df = pd.read_csv(f"{path}")
+    df.drop(columns=["Unnamed: 0"], inplace=True, errors='ignore')
+    return df
+
+# Section3 - Modelling
+def load_data(path: str = "/path/to/csv/"):
+    """
+    This function takes a path string to a CSV file and loads it into
+    a Pandas DataFrame.
+
+    :param      path (optional): str, relative path of the CSV file
+
+    :return     df: pd.DataFrame
+    """
+
+    df = pd.read_csv(f"{path}")
+    df.drop(columns=["Unnamed: 0"], inplace=True, errors='ignore')
+    return df
